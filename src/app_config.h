8ea314f18d97221245af5ff0d66c09df0f9daba9
Renjith I S <mailrenjithis@gmail.com>
2015-01-17 14:06:55 +0530

    Added config sample
    Need to set config URL and create page

---
+++ src/app_config.h
@@ -0,0 +1,56 @@
+#include <pebble.h>
+
+#define KEY_DATA 5
+  
+void *config_changed_callback(DictionaryIterator *config_iterator);
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+//   // Get the first pair
+//   Tuple *t = dict_read_first(iterator);
+
+//   // Process all pairs present
+//   while (t != NULL) {
+//     // Long lived buffer
+//     static char s_buffer[64];
+
+//     // Process this pair's key
+//     switch (t->key) {
+//       case KEY_DATA:
+//         // Copy value and display
+//         snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", t->value->cstring);
+//         text_layer_set_text(s_output_layer, s_buffer);
+//         break;
+//     }
+
+//     // Get next pair, if any
+//     t = dict_read_next(iterator);
+//   }
+  
+  config_changed_callback(iterator);
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+  
+void config_init(void *config_changed) {
+  
+  // Register callbacks
+app_message_register_inbox_received(inbox_received_callback);
+app_message_register_inbox_dropped(inbox_dropped_callback);
+app_message_register_outbox_failed(outbox_failed_callback);
+app_message_register_outbox_sent(outbox_sent_callback);
+
+
+}
+
+
+
---
+++ src/pebble-js-app.js
@@ -0,0 +1,39 @@
+
+var CONFIG_URL = "http://google.com"
+
+Pebble.addEventListener('ready',
+  function(e) {
+    console.log('JavaScript app ready and running!');
+  }
+);
+
+
+  // register showConfiuration event listner
+Pebble.addEventListener('showConfiguration', function(e) {
+  // Show config page
+  Pebble.openURL(CONFIG_URL);
+});
+
+
+// register webviewclosed event listner
+Pebble.addEventListener('webviewclosed',
+function(e) {
+    console.log('Configuration window returned: ' + e.response);
+    
+      // Construct a dictionary
+  var dict = {
+    'KEY_DATA':'Hello from PebbleKit JS!'
+  };
+
+  console.log(dict);
+  
+  // Send a string to Pebble
+  Pebble.sendAppMessage(dict,
+    function(e) {
+      console.log('Send successful.');
+    },
+    function(e) {
+      console.log('Send failed!');
+    }
+  );
+});
